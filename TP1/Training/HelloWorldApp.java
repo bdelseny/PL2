4a05e07370ae4cd3192b27662583fd6f36f0837c
bdelseny <bd_31@hotmail.fr>
2016-11-29 12:41:55 +0100

    Ajout du TP1

---
+++ TP1/Training/HelloWorldApp.java
@@ -0,0 +1,10 @@
+/**
+* The HelloWorldApp class implements an application that
+* simply prints "Hello Wolrd!" to standard output.
+*/
+
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		System.out.println("BONJOUR\n CECI EST MON \n PREMIER PROGRAMME JAVA"); // Display string.
+	}
+}
