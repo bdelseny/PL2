e9169f5e62260d34a15bb9d7df50c6d0fbb51d24
bdelseny <bd_31@hotmail.fr>
2016-12-01 11:24:48 +0100

    Création classe EnsembleDeLettres

---
+++ TP5/EnsembleDeLettres/src/ensembledelettres/EnsembleDeLettres.java
@@ -0,0 +1,34 @@
+
+package ensembledelettres;
+
+/**
+ * Ensemble de lettres non duppliquées
+ * 
+ * @author delsenyb
+ */
+public class EnsembleDeLettres {
+    private String[] e;
+    
+    /**
+     * Construit un ensemble de lettre vide
+     */
+    public EnsembleDeLettres() {
+    }
+    
+    /**
+     * Construit un ensemble de lettres de i lettres
+     * @param i  nombre de lettres (0<i<=26)
+     */
+    public EnsembleDeLettres(int i) {
+    }
+
+    /**
+     * Construit un ensemble de lettres suivant les lettres choisis
+     * @param a lettres en entrées
+     */
+    public EnsembleDeLettres(String[] a) {
+        // Tester si répétition de lettres dans a
+    }
+    
+    
+}
