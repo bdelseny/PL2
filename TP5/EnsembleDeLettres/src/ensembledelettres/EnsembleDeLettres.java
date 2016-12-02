81bec970723e32bf337f7fade27a6c63afc699f2
bdelseny <bd_31@hotmail.fr>
2016-12-02 08:40:36 +0100

    Ajout fonctions toString, estVide, cardinal, inclusion, estPresente

--- TP5/EnsembleDeLettres/src/ensembledelettres/EnsembleDeLettres.java
+++ TP5/EnsembleDeLettres/src/ensembledelettres/EnsembleDeLettres.java
@@ -24,10 +24,53 @@ public class EnsembleDeLettres {
 
     /**
      * Construit un ensemble de lettres suivant les lettres choisis
-     * @param a lettres en entrées
+     * @param a  lettres en entrées
      */
     public EnsembleDeLettres(String[] a) {
         // Tester si répétition de lettres dans a
+    }
+    
+    /**
+     * Affiche l'ensemble de lettres
+     * @return {e,n,s,e,m,b,l,e}
+     */
+    @Override
+    public String toString() {
+        return "{" + "}";
+    }
+    
+    /**
+     * Test si l'ensemble est vide
+     * @return booléen  vrai si ensemble vide
+     */
+    public boolean estVide(){
+        return ;
+    }
+    
+    /**
+     * Cardinal de l'ensemble (nombre d'éléments)
+     * @return entier
+     */
+    public int cardinal(){
+        return ;
+    }
+    
+    /**
+     * Test l'inclusion de l'ensemble dans un autre ensemble
+     * @param e1
+     * @return booléen  vrai si l'ensemble est inclut dans l'autre ensemble
+     */
+    public boolean inclusion(EnsembleDeLettres e1){
+        return ;
+    }
+    
+    /**
+     * Test si la lettre spécifiée est présente dans l'ensemble
+     * @param l
+     * @return booléen  vrai si la lettre est présente dans l'ensemble
+     */
+    public boolean estPresente(char l){
+        return ;
     }
     
     
