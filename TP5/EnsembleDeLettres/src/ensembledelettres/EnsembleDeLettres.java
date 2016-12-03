e5c6fa949dd563604ebb4d197585f48ad13c0ef2
bdelseny <bd_31@hotmail.fr>
2016-12-03 13:32:52 +0100

    TP5

--- TP5/EnsembleDeLettres/src/ensembledelettres/EnsembleDeLettres.java
+++ TP5/EnsembleDeLettres/src/ensembledelettres/EnsembleDeLettres.java
@@ -1,115 +1,163 @@
-
 package ensembledelettres;
 
 /**
  * Ensemble de lettres non duppliquées
- * 
+ *
  * @author delsenyb
  */
 public class EnsembleDeLettres {
-    private String[] e;
-    
+
+    private boolean[] present;
+
     /**
-     * Construit un ensemble de lettre vide
+     * Converti un caractère en entier
+     *
+     * @param a le caractère à convertir
+     * @return b l'entier qui correspond à a
      */
-    public EnsembleDeLettres() {
+    private int charToInt(char a) {
+        int b;
+        b = (int) a - 97;
+        return b;
     }
-    
+
     /**
-     * Construit un ensemble de lettres de i lettres
-     * @param i  nombre de lettres (0<i<=26)
+     * Converti un entier en caractère
+     *
+     * @param a l'entier à convertir
+     * @return b le caractère correspondant à l'indice a
      */
-    public EnsembleDeLettres(int i) {
+    private char intToChar(int a) {
+        char b;
+        b = (char) (a + 97);
+        return b;
+    }
+
+    /**
+     * Construit un ensemble de lettre qui peut être vide ou aléatoire
+     *
+     * @param vide vrai le tableau est vide
+     */
+    public EnsembleDeLettres(boolean vide) {
+        present = new boolean[26];
+        if (!vide) {
+            int a;
+            for (int i = 0; i < 26; i++) {
+                present[i] = Math.round(Math.random()) == 1;
+            }
+        }
     }
 
     /**
      * Construit un ensemble de lettres suivant les lettres choisis
-     * @param a  lettres en entrées
+     *
+     * @param a lettres en entrées
      */
-    public EnsembleDeLettres(String[] a) {
-        // Tester si répétition de lettres dans a
+    public EnsembleDeLettres(String a) {
+        this(true);
+        int x;
+        for (int i = 0; i < a.length(); i++) {
+            x = charToInt(a.charAt(i));
+            present[x] = true;
+        }
     }
-    
+
     /**
      * Affiche l'ensemble de lettres
-     * @return {e,n,s,e,m,b,l,e}  l'ensemble de lettres
+     *
+     * @return {e,n,s,e,m,b,l,e} l'ensemble de lettres
      */
     @Override
     public String toString() {
-        return "{" + "}";
+        char lettre;
+        String ensemble = new String();
+        for (int i = 0; i < 26; i++) {
+            if (present[i]) {
+                lettre = intToChar(i);
+                ensemble = ensemble + lettre + ",";
+            }
+        }
+        return "{" + ensemble + "}";
     }
-    
+
     /**
      * Test si l'ensemble est vide
-     * @return booléen  vrai si ensemble vide
+     *
+     * @return booléen vrai si ensemble vide
      */
-    public boolean estVide(){
+    public boolean estVide() {
         return false;
     }
-    
+
     /**
      * Cardinal de l'ensemble (nombre d'éléments)
-     * @return entier  cardinal de l'ensemble
+     *
+     * @return entier cardinal de l'ensemble
      */
-    public int cardinal(){
+    public int cardinal() {
         return 0;
     }
-    
+
     /**
      * Test l'inclusion de l'ensemble dans un autre ensemble
-     * @param e  l'ensemble à tester
-     * @return booléen  vrai si l'ensemble est inclut dans l'autre ensemble
+     *
+     * @param e l'ensemble à tester
+     * @return booléen vrai si l'ensemble est inclut dans l'autre ensemble
      */
-    public boolean inclusion(EnsembleDeLettres e){
+    public boolean inclusion(EnsembleDeLettres e) {
         return false;
     }
-    
+
     /**
      * Test si la lettre spécifiée est présente dans l'ensemble
-     * @param l  lettre à tester
-     * @return booléen  vrai si la lettre est présente dans l'ensemble
+     *
+     * @param l lettre à tester
+     * @return booléen vrai si la lettre est présente dans l'ensemble
      */
-    public boolean estPresente(char l){
+    public boolean estPresente(char l) {
         return false;
     }
-    
+
     /**
-     * Génère l'ensemble d'intersection entre
-     * l'ensemble courant et un autre ensemble
-     * @param e  l'autre ensemble
+     * Génère l'ensemble d'intersection entre l'ensemble courant et un autre
+     * ensemble
+     *
+     * @param e l'autre ensemble
      * @return EnsembleDeLettres est l'intersection des deux ensembles
      */
-    public EnsembleDeLettres creerIntersection(EnsembleDeLettres e){
+    public EnsembleDeLettres creerIntersection(EnsembleDeLettres e) {
         return null;
     }
-    
+
     /**
-     * Génère un ensemble d'union entre
-     * l'ensemble courant et un autre ensemble
-     * @param e  l'autre ensemble
+     * Génère un ensemble d'union entre l'ensemble courant et un autre ensemble
+     *
+     * @param e l'autre ensemble
      * @return EnsembleDeLettres est l'union des deux ensembles
      */
-    public EnsembleDeLettres creerUnion(EnsembleDeLettres e){
+    public EnsembleDeLettres creerUnion(EnsembleDeLettres e) {
         return null;
     }
-    
+
     /**
-     * Génère un ensemble de différence entre
-     * l'ensemble courant et un autre ensemble
-     * @param e  l'autre ensemble
+     * Génère un ensemble de différence entre l'ensemble courant et un autre
+     * ensemble
+     *
+     * @param e l'autre ensemble
      * @return EsembleDeLettres est la différence des deux ensembles
      */
-    public EnsembleDeLettres creerDifference(EnsembleDeLettres e){
+    public EnsembleDeLettres creerDifference(EnsembleDeLettres e) {
         return null;
     }
-    
+
     /**
-     * Génère un ensemble d'union disjointe entre
-     * l'ensemble courant et un autre ensemble
-     * @param e  l'autre ensemble
+     * Génère un ensemble d'union disjointe entre l'ensemble courant et un autre
+     * ensemble
+     *
+     * @param e l'autre ensemble
      * @return EsembleDeLettres est l'union disjointe des deux ensembles
      */
-    public EnsembleDeLettres creerUnionDisjointe(EnsembleDeLettres e){
+    public EnsembleDeLettres creerUnionDisjointe(EnsembleDeLettres e) {
         return null;
     }
 }
--- TP5/EnsembleDeLettres/src/ensembledelettres/Test.java
+++ TP5/EnsembleDeLettres/src/ensembledelettres/Test.java
@@ -5,6 +5,9 @@
  */
 package ensembledelettres;
 
+import ufrim2ag.m2pcci.pl2.tp3.EnsembleDeLettres1;
+
+
 /**
  *
  * @author delsenyb
@@ -14,8 +17,40 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        EnsembleDeLettres e;
-        e = new EnsembleDeLettres();
+        
+        
+        // Eensemble eV vide
+        EnsembleDeLettres eV;
+        eV = new EnsembleDeLettres(true);
+        System.out.println("eV : {} " + eV);
+        
+        // Ensemble e1 azerty
+        EnsembleDeLettres e1;
+        e1 = new EnsembleDeLettres("azertyztzza");
+        System.out.println("e1 : " + e1);
+        
+        // Ensemble e2 qwerty
+        EnsembleDeLettres1 e2;
+        e2 = new EnsembleDeLettres1("qwertyyyrr");
+        System.out.println("e2 : " + e2);
+        
+        // Enesemble e3 xcvb
+        EnsembleDeLettres1 e3;
+        e3 = new EnsembleDeLettres1("xcvb");
+        System.out.println("e3 : " + e3);
+        
+//        // Intersection e1 et e2
+//        System.out.println("Intersection e1 et e2 : {b,c,v,x} :");
+//        System.out.println(e1.intersection1(e2));
+//        
+//        // Intersection e1 et e3
+//        System.out.println("Intersection e1 et e3 : {} :");
+//        System.out.println(e1.intersection1(e3));
+//        
+//        // Intersection e1, e2 et e3
+//        System.out.println("Union e1, e2 et e3 : {a,b,c,e,q,r,t,v,w,x,y,z} :");
+//        System.out.println(e1.union(e2.union(e3)));
+        
+        
     }
 }
